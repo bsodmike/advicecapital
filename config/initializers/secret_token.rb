397b12c3cedd13a734be23721c3d5661c58e4da9
Daniel <znowm4n@gmail.com>
2011-02-24 23:35:52 +0100

    created new app advicecapital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Advicecapital::Application.config.secret_token = '557ffa19af1e551b569371e40fad5e22c296ea750311509131d1b39026872638788e5d5bc1c62501a55e9a9856af2c400b33d416ca6eb1975639488337ecf3ce'
