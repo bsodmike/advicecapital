78ab9092dbb7423e1fe7cb1d9a584e301afab62d
Daniel <znowm4n@gmail.com>
2011-04-20 13:08:25 +0200

    update

--- app/models/employee.rb
+++
@@ -1,9 +0,0 @@
-class Employee < ActiveRecord::Base
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # full regex
-
-  validates :name, :presence => true
-  validates :title, :presence => true
-  validates :email, :presence => true, :format => { :with => email_regex }
-  validates :phone, :presence => true
-  validates :image, :presence => true
-end
