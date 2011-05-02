6d9a42fbb768b8f921063a4b25de2293475ac47b
Daniel <znowm4n@gmail.com>
2011-05-02 14:04:44 +0200

    updated index page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,24 +1,32 @@
 (function ($) {
-	$(function () {
-		$('nav ul li a').mouseover(function () {
-			$(this).siblings('li ul').addClass('hover');
-			
-			$(this).parent().hover(
-				function () {},
-				function () {
-					$(this).children('ul').removeClass('hover');
-				})
-		},
-		function () {
-		});
-    $("#slider").jCarouselLite({
+  $(function () {
+    $('nav ul li a').mouseover(function () {
+      $(this).siblings('li ul').addClass('hover');
+
+      $(this).parent().hover(
+        function () {},
+        function () {
+          $(this).children('ul').removeClass('hover');
+        })
+      },
+      function () {
+      });
+    
+    $(".slider").jCarouselLite({
       auto: 1,
       speed: 1800,
-      visible: 2,
+      visible: 4,
       btnNext: ".arrow_right",
       btnPrev: ".arrow_left"
     });
-	});
+
+    $(".news_ticker").jCarouselLite({
+      vertical: true,
+      auto: 800,
+      speed: 2000,
+      visible: 10
+    });
+  });
 })(jQuery);
 
   
