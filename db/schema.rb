ebb6cfc4e90ac9d32a518014d692e193dfa82dcf
Daniel <znowm4n@gmail.com>
2011-05-05 23:53:53 +0200

    added seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110415145526) do
+ActiveRecord::Schema.define(:version => 20110505212659) do
+
+  create_table "advice_pages", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "boards", :force => true do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Employee.create([
+    {
+      :name => 'Lars Fetterlein',
+      :title => 'Managing Partner',
+      :email => 'lf@advicecapital.dk',
+      :phone => '+45 2216 4081',
+      :image => 'lars_fetterlein.jpg'
+    },
+    {
+      :name => 'Tobias Hvilsted',
+      :title => 'IT & Administration',
+      :email => 'th@advicecapital.dk',
+      :phone => '+45 4224 6134',
+      :image => 'tobias_hvilsted.jpg'
+    },
+    {
+      :name => 'Linda Kops',
+      :title => 'Sekret&aelig;r',
+      :email => 'lk@advicecapital.dk',
+      :phone => '+45 3930 6080',
+      :image => 'lind_kops.jpg'
+    },
+    {
+      :name => 'Anette Vinum',
+      :title => 'Receptionist',
+      :email => 'info@advicecapital.dk',
+      :phone => '+45 3930 6080',
+      :image => 'anette.jpg'
+    },
+    {
+      :name => 'Frederik Foged Dreyer-Nielsen',
+      :title => 'Bestyrelsesmedlem i Advice Invest',
+      :email => 'ffdn@advicecapital.dk',
+      :phone => '+45 2618 9817',
+      :image => 'frederik.jpg'
+    },
+    {
+      :name => 'Jesper Thaning',
+      :title => 'Bestyrelsesmedlem i Advice Invest',
+      :email => 'jh@advicecapital.dk',
+      :phone => '+45 4013 7654',
+      :image => 'jesper_thaning.jpg'
+    }
+])
