c2362047c9520f8f99d88a7f63f6b410ba508dcd
Daniel <znowm4n@gmail.com>
2011-07-16 19:48:22 +0200

    updated db migrations

--- db/migrate/20110414200609_add_image_to_employees.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToEmployees < ActiveRecord::Migration
-  def self.up
-    add_column :employees, :image, :string
-  end
-
-  def self.down
-    remove_column :employees, :image
-  end
-end
