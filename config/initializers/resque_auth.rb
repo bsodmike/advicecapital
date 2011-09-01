b8932242e48c099651ea1f5f621760824148d91c
Michael de Silva <michael@mwdesilva.com>
2011-09-02 02:18:27 +0300

    corrected mistake in if block

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,5 +1,5 @@
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
-  if ['admin'].include? user do
+  if ['admin'].include? user
   "$2a$10$v8xObseXNyjTgSpAWDQFDupX6Px1b8xxPylZQOBq2n9ZOom37fg5C" == BCrypt::Engine.hash_secret(password, "$2a$10$v8xObseXNyjTgSpAWDQFDu")
   end
 end
