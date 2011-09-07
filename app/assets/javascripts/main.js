f49c4f57015a02e754691e73bf75635b42485ca3
Daniel <znowm4n@gmail.com>
2011-09-07 11:29:03 +0200

    updated scripts and stylesheets and views

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -8,6 +8,24 @@
       });
     });
 
+    google.load("maps","2.x");
+
+    function initialize() {
+      if (GBrowserIsCompatible()) {
+        var map = new GMap2(document.getElementById("map"));
+        map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+        map.setUIToDefault();
+      }
+    }
+
+
+
+
+
+
+
+
+
     tinyMCE.init({
         mode : "textareas",
         theme: "advanced",
