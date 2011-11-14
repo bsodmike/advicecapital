cf1012be32a9e2eaf512d00344b0656ccbf6b63f
Daniel G <znowm4n@gmail.com>
2011-11-14 16:46:44 +0100

    updated Panda gem script, video uploading is now working

--- db/schema.rb
+++ db/schema.rb
@@ -93,14 +93,14 @@ ActiveRecord::Schema.define(:version => 20111106134016) do
     t.string   "name"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
   create_table "videos", :force => true do |t|
     t.string   "title"
     t.string   "panda_video_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
