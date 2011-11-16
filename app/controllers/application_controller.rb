2bd38185388176fbe7605f0c9d93a32dd82445d7
Daniel G <znowm4n@gmail.com>
2011-11-16 16:40:24 +0100

    deactivated force_ssl for now, until certs are configured on host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  force_ssl
+  #force_ssl
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Du har ikke rettigheder til dette."
     redirect_to root_url
