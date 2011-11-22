d8dacce223af8477c09abbf3799c8d63dee279b4
Daniel <znowm4n@gmail.com>
2011-11-22 20:53:38 +0100

    removed registrable from deviser user model, so signing up is disabled, updated users index view listing, its complete, updated users controller, need the last methods

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,11 +5,25 @@ class Admin::UsersController < AdminController
 		@users = User.all # Shows all users except current_users
 	end
 
-	def new
+	def show
+		# @user = current_user
+		@user = User.find(params[:id])
 	end
 
-	def show
-		@user = current_user
+	def new
+		User.new
 	end
+
+	def create
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if @user.destroy
+			redirect_to admin_users_path, :notice => "Brugeren blev slettet."
+		end
+	end
+
+	
 
 end
