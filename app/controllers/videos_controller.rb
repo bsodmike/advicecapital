5046892d75f48371b5fd8d11ef30e74499bdeca2
Daniel <znowm4n@gmail.com>
2011-11-23 19:38:53 +0100

    renamed users views to devise, so it works, updating several views with cancan and such

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -22,4 +22,10 @@ class VideosController < ApplicationController
     redirect_to :action => :show, :id => @video.id
   end
 
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+
+    redirect_to videos_path, :notice => "Video blev slettet."
+
 end
