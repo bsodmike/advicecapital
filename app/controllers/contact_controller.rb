6db622070c940b3ab8559b5de963e7299587c6c6
Daniel G <znowm4n@gmail.com>
2012-01-04 14:31:19 +0100

    got contact page email sending working

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,18 +4,8 @@ class ContactController < ApplicationController
     @message = Message.new
   end
 
-  # def create
-  #   @message = Message.new(params[:message])
-  #    if @message.valid?
-  #     Contact.contact(@message).deliver
-  #     redirect_to root_path, :notice => "Din email blev sendt, vi kontakter dig."
-  #   else
-  #     render :new, :error => "Din email blev ikke sendt."
-  #   end
-  # end
-
   def create
-    if Contact.contact(params[:message][:message], params[:message][:email]).deliver
+    if Contact.contact(params[:message][:message], params[:message][:email], params[:message][:name]).deliver
       redirect_to root_path, :notice => "Din email blev sendt, vi kontakter dig."
     else
       render :new, :error => "Din email blev ikke sendt."
--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -2,14 +2,14 @@
 class Contact < ActionMailer::Base
   #default :to => "contact@advicecapital.dk"
 
-  def contact(message, sender)
+  def contact(message, sender, name)
     @message = message
     @sender = sender
+    @name = name
     mail(
       :from => sender,
       :to => "znowm4n@gmail.com",
-      :subject => "[Contact AC]",
-      :message => message
+      :subject => "[Contact AC]"
     )
   end
 end
