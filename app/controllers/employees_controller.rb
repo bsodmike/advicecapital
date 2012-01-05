b883f09a4643fe141704ebc4cd7b713766013f04
Daniel G <znowm4n@gmail.com>
2012-01-05 13:05:28 +0100

    updated employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,6 +3,7 @@ class EmployeesController < ApplicationController
   def index
     @employees = Employee.all
     respond_to do |format|
+      format.html
       format.json  { render :json => @employees }
     end
   end
