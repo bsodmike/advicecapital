f516ed1b3c2edeaee1a079c1f248c95ef607a480
Daniel G <znowm4n@gmail.com>
2012-01-11 22:05:38 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Advicecapital::Application.routes.draw do
   match '/receive_news', :to => 'pages#receive_news'
   
   match '/disclaimer', :to => 'pages#disclaimer'
+  match '/privacy', :to => 'pages#privacy'
   match '/organisation', :to => 'employees#index'
 
   mount Resque::Server, :at => "/resque"
