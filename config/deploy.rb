5ea310e2f9f0bbcf3bb7c173e36354a1f4d5a44d
Daniel G <znowm4n@gmail.com>
2012-02-23 13:25:05 +0100

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,14 +7,14 @@ set :branch, "master"
 
 set :deploy_via, :remote_cache
 
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
-
 set :application, "advicecapital"
 set :deploy_to , "/var/www/apps/#{application}"
 set :user, "deploy"
 set :use_sudo, true
 set :admin_runner, "root"
+
+set :whenever_command, "bundle exec whenever"
+require "whenever/capistrano"
 
 role :web, "danielg.dk"                          # Your HTTP server, Apache/etc
 role :app, "danielg.dk"                          # This may be the same as your `Web` server
