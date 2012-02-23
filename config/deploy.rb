66e25ac9167627b8086ca075da754a31bbd2e409
Daniel G <znowm4n@gmail.com>
2012-02-23 09:42:45 +0100

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :branch, "master"
 
 set :deploy_via, :remote_cache
 
-set :whenever_command, "bundle exec whenever"#"/usr/local/rvm/gems/ruby-1.9.3-p0/bin/whenever"
+set :whenever_command, "/usr/local/rvm/gems/ruby-1.9.3-p0/bin/bundle exec whenever"
 require "whenever/capistrano"
 
 set :application, "advicecapital"
