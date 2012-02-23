4ddfe3bf9faed20753f498244b25eb6f6ead6368
Daniel G <znowm4n@gmail.com>
2012-02-23 09:07:33 +0100

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-require "whenever/capistrano"
-
 default_run_options[:pty] = true
 set :repository,  "git@github.com:Znow/advicecapital.git"
 
@@ -9,7 +7,8 @@ set :branch, "master"
 
 set :deploy_via, :remote_cache
 
-set :whenever_command, "/usr/local/rvm/gems/ruby-1.9.3-p0/bin/whenever"
+require "whenever/capistrano"
+#set :whenever_command, "/usr/local/rvm/gems/ruby-1.9.3-p0/bin/whenever"
 
 set :application, "advicecapital"
 set :deploy_to , "/var/www/apps/#{application}"
