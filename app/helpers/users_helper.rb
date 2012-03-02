3fbfad60e5c21ccc224d966f4908e13a28a0ef2f
Daniel G <znowm4n@gmail.com>
2012-03-02 13:40:30 +0100

    updated users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
+# encoding: UTF-8
 module UsersHelper
+
+  def user_role(role)
+    if role == "super_admin"
+      "Super Admininistrator"
+    elsif role == "admin"
+      "Administrator"
+    elsif role == "investor"
+      role.capitalize
+    elsif role == "visitor"
+      "Gæst"
+    end
+  end
 end
