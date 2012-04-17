9173ce69a5a9d70b7773d6fbdfb915f90ca50f6c
Daniel <znowm4n@gmail.com>
2012-04-17 20:17:19 +0200

    updating application controller for locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,18 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
   
+  before_filter :set_locale
+  
 
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
   end
+  
+private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
 end
