149a484e4a13819d3ac763d65d2d5ab949b548a5
Daniel Gottschalck <znowm4n@gmail.com>
2012-07-11 23:26:22 +0200

    updated locale translation in advice page model, updated show span to span-15

--- app/models/advice_page.rb
+++ app/models/advice_page.rb
@@ -4,4 +4,9 @@ class AdvicePage < ActiveRecord::Base
   attr_accessible :title, :content, :template, :slug
   
   translates :title, :content
+
+  class AdvicePage::Translation
+  	attr_accessible :locale
+  end
+
 end
