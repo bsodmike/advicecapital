37a4b4bfc7e9ef91099f78479cbb57b2a4b6db1f
Daniel Gottschalck <znowm4n@gmail.com>
2012-09-13 21:00:37 +0200

    updated stocks controller, model and views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,4 +5,6 @@ class Stock < ActiveRecord::Base
 	attr_accessible :value, :month, :year
 
 	accepts_nested_attributes_for :investor_stocks, :allow_destroy => true
+
+	validates :value, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than => 0, :less_than => 9999}
 end
