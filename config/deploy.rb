58669b296a77591657d44caf173190b9c13234b4
Daniel Gottschalck <znowm4n@gmail.com>
2012-10-13 10:28:15 +0200

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,10 +24,10 @@ role :db,  "znow.dk", :primary => true        # This is where Rails migrations w
 
 
 #If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
-end
+#namespace :deploy do
+   #task :start do ; end
+   #task :stop do ; end
+   #task :restart, :roles => :app, :except => { :no_release => true } do
+     #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   #end
+#end
