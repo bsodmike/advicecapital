6f69f53d345af21f1fd6228873cafe601baf9346
Daniel Gottschalck <znowm4n@gmail.com>
2012-10-20 13:28:19 +0200

    update

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,7 +7,6 @@ class Employee < ActiveRecord::Base
   validates :title, :presence => true
   validates :email, :presence => true, :format => { :with => email_regex }
   validates :phone, :presence => true
-  validates :image, :presence => true
 
   def as_json(options={})
     { :id => self.id, :name => self.name, :title => self.title, :email => self.email, :phone => self.phone, :image => self.image }
